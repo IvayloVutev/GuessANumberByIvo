d7b77e0450deaa585f52b4cb25d2b185b459a1b0
IvayloVutev <122825073+IvayloVutev@users.noreply.github.com>
2023-01-29 19:47:52 +0200

    Added functionality for restarting the game, and functionality for difficulty levels

--- GuessANumber.cs
+++ GuessANumber.cs
@@ -8,10 +8,41 @@ namespace GuessANumber
         {
             Random randomNumber = new Random();
             int computerNumber = randomNumber.Next(1, 101);
+            int countTheTryes = 0;
 
+            Console.Write("Choose a level of difficulty - [easy] [medium] [hard]: ");
+            string difficult = Console.ReadLine();
+            Difficult(difficult);
+            int numberOfgames = Difficult(difficult);
+  
+            Console.WriteLine($"You have {numberOfgames} tryes to guess the number");
+            Console.WriteLine();
+            
             while (true)
             {
-                Console.Write("Guess a number (1-100): ");
+                countTheTryes++;
+
+                if (countTheTryes <= numberOfgames)
+                {
+                    Console.Write("Guess a number (1-100): ");
+                }
+                else if (countTheTryes > numberOfgames)
+                {
+                    Console.WriteLine("Game over");
+                    Console.Write("Type [yes] to Play Again or [no] to quit: ");
+                    string input = Console.ReadLine();
+
+                    if (input == "yes")
+                    {
+                        countTheTryes = 0;
+                        continue;
+                    }
+                    else if (input == "no")
+                    {
+                        break;
+                    }
+                }
+ 
                 string playerInput = Console.ReadLine();
                 bool isValid = int.TryParse(playerInput, out int playerNumber);
 
@@ -36,6 +67,29 @@ namespace GuessANumber
                     Console.WriteLine("Invalid Input.");
                 }
             }
+        }
+
+        static int Difficult(string text)
+        {          
+            int numberOfgames = 0;
+            if (text == "easy")
+            {
+                numberOfgames = 11;
+            }
+            else if (text == "medium")
+            {
+                numberOfgames = 7;
+            }
+            else if (text == "hard")
+            {
+                numberOfgames = 5;
+            }
+            else
+            {
+                Console.WriteLine("Invalid level");
+                
+            }
+            return numberOfgames;
         }
     }
 }
